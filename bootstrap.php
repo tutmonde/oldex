89d620a951aa00c6e1f10c2278748bb8930a6c2f
veselcraft <veselcraft@icloud.com>
2021-08-07 17:13:44 +0300

    New define OLDEX_VERSION for reporting their Git version

--- bootstrap.php
+++ bootstrap.php
@@ -6,4 +6,11 @@ function searchWithDDG(string $input): Array
     return (new SearchEngine)->searchWithDDG($input);
 }
 
-return (function() {});
+return (function() {
+    if(is_dir($gitDir = OLDEX_ROOT . "/.git"))
+        $ver = trim(`git --git-dir="$gitDir" log --pretty="%h" -n1 HEAD` ?? "Unknown version");
+    else
+        $ver = "Unknown version (git)";
+
+    define('OLDEX_VERSION', 'Perlence '.$ver, false);
+});
